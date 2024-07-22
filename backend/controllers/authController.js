62cf14114cea17d93eb549c189fc47c30c740a3c
pawan-chauhan-9560 <pawanchauhan933@gmail.com>
2024-07-22 23:46:12 +0530

    added package bcrypt

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,9 +1,9 @@
 import User from '../models/userModel.js';
+import bcryptjs from 'bcryptjs';
 export const signup = async (req, res) => {
-    console.log(req.body);
-
     const {username,email,password}=req.body;
-    const newUser=new User({username,email,password});
+    const hashPassword= await bcryptjs.hashSync(password,10);
+    const newUser= new User({username,email,password:hashPassword});
     await newUser.save();
     res.status(201).json('record saved');
     console.log(username);
