fd3947b0d07782e9fa8937a8269f38ce11142de7
pawan-chauhan-9560 <pawanchauhan933@gmail.com>
2024-07-25 00:03:07 +0530

    Create a middleware to handle errors

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = await bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashPassword });
@@ -8,7 +8,7 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json("record saved");
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
   console.log(username);
 };
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/userRoute.js';
-import authRouter from './routes/authRoute.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+import authRouter from "./routes/authRoute.js";
 
 dotenv.config();
 
@@ -11,23 +11,34 @@ const PORT = process.env.PORT || 8000;
 const MONGO_URI = process.env.MONGO;
 
 if (!MONGO_URI) {
-    console.error("MongoDB connection string is not defined in environment variables");
-    process.exit(1);
+  console.error(
+    "MongoDB connection string is not defined in environment variables"
+  );
+  process.exit(1);
 }
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("Database connected successfully");
-    })
-    .catch((err) => {
-        console.error("Database connection error:", err);
-        process.exit(1);
-    });
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+    process.exit(1);
+  });
 app.use(express.json());
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
-app.use('/backend/auth',authRouter);
-app.use('/backend/user',userRouter);
-
+app.use("/backend/auth", authRouter);
+app.use("/backend/user", userRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode ?? 500;
+  const statusMsg = err.message ?? `Internal Server Error`;
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    statusMsg,
+  });
+});
